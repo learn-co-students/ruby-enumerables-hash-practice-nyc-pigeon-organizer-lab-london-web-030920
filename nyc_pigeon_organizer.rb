4610ec5ccce9f322b2c1eb6fa7a112f3af96a39c
premti <premti@gmail.com>
2020-03-05 15:16:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  new_array = []
+    data.each do |key, value|
+      value.each do |color, name| 
+        name.each do |names|
+          if !new_hash[names] 
+             new_hash[names] = {}
+          end
+          if !new_hash[names][key]
+            new_hash[names][key] = []
+          end
+          new_hash[names][key].push(color.to_s)
+        end
+      end
+  end
+  new_hash
 end
